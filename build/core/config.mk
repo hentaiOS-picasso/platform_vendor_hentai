a1c9daf05d3579cb40a27a68de8da68773773ca1
XayahSuSuSu <zds1249475336@gmail.com>
2022-05-05 01:30:47 +0800

    Revert "build: core: Ignore neverallows"
    This reverts commit c81337a8c8c20a1ea70ed8d3e717e0155475ec30.
    
    Change-Id: I7f6775cff2c36a6e5f8b88471d8243962ea739cb

--- build/core/config.mk
+++ build/core/config.mk
@@ -15,6 +15,3 @@
 
 # Rules for QCOM targets
 include $(TOPDIR)vendor/hentai/build/core/qcom_target.mk
-
-# We modify several neverallows, so let the build proceed
-SELINUX_IGNORE_NEVERALLOWS := true
