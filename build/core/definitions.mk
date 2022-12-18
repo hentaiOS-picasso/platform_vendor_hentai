CSC-UI/android_vendor_csc

build: Reorganize inclusion of QCOM macros
* There's actually not a whole lot QC-specific about these macros,
  make them available at definition.mk inclusion time rather than
  relying on BoardConfig to include them.
* Don't create a dependency on QCOM macros from a makefile
  that is included very early in the build process.

Change-Id: I3dcaf0e068ca44732cfe58f69384621781ddda67
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

build/core/definitions.mk
@@ -1,0 +1,2 @@
+# Include QCOM macros
+include vendor/acme/build/core/qcom_utils.mk

hentaiOS-picasso/platform_vendor_hentai

build/core/definitions.mk
@@ -1,5 +1,4 @@
-# Include board/platform macros
-include vendor/hentai/build/core/utils.mk
-
 # Include vendor platform definitions
 include vendor/hentai/build/core/vendor/*.mk
+# Include QCOM macros
+include vendor/hentai/build/core/qcom_utils.mk
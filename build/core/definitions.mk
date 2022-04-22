bd490d505acc634d22cda01658aa55b8287f6d3b
Ethan Chen <intervigil@gmail.com>
2022-04-22 17:31:35 +0800

    build: Reorganize inclusion of QCOM macros
    * There's actually not a whole lot QC-specific about these macros,
      make them available at definition.mk inclusion time rather than
      relying on BoardConfig to include them.
    * Don't create a dependency on QCOM macros from a makefile
      that is included very early in the build process.
    
    Change-Id: I3dcaf0e068ca44732cfe58f69384621781ddda67
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -1,5 +1,4 @@
-# Include board/platform macros
-include vendor/hentai/build/core/utils.mk
-
 # Include vendor platform definitions
 include vendor/hentai/build/core/vendor/*.mk
+# Include QCOM macros
+include vendor/hentai/build/core/qcom_utils.mk
