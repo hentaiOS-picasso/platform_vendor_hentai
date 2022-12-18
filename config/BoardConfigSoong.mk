CSC-UI/android_vendor_csc

build: Rename fod to udfps
 * now flag is changed to TARGET_SURFACEFLINGER_UDFPS_LIB from TARGET_SURFACEFLINGER_FOD_LIB

Change-Id: I499b31d00a053b865ceaa3b34a9762f980bbf14e

config/BoardConfigSoong.mk
@@ -32,7 +32,7 @@ SOONG_CONFIG_acmeGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
-    target_surfaceflinger_fod_lib
+    target_surfaceflinger_udfps_lib
 
 SOONG_CONFIG_NAMESPACES += acmeQcomVars
 SOONG_CONFIG_acmeQcomVars += \
@@ -55,14 +55,14 @@ SOONG_CONFIG_acmeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
-TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_acmeGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_acmeGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_acmeGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_acmeGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
-SOONG_CONFIG_acmeGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
+SOONG_CONFIG_acmeGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_acmeQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
 endif

hentaiOS-picasso/platform_vendor_hentai

config/BoardConfigSoong.mk
@@ -32,7 +32,7 @@ SOONG_CONFIG_hentaiGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
-    target_surfaceflinger_fod_lib
+    target_surfaceflinger_udfps_lib
 
 SOONG_CONFIG_NAMESPACES += hentaiQcomVars
 SOONG_CONFIG_hentaiQcomVars += \
@@ -55,14 +55,14 @@ SOONG_CONFIG_hentaiQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PR
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
-TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_hentaiGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_hentaiGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_hentaiGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_hentaiGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
-SOONG_CONFIG_hentaiGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
+SOONG_CONFIG_hentaiGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_hentaiQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
 endif