CSC-UI/android_vendor_csc

build: soong: Dynamically generate kernel headers using acme generator
Add a soong vendor plugin for kernel config variables so we can use
these in go.

Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6

build/soong/generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func acmeExpandVariables(ctx android.ModuleContext, in string) string {
+	acmeVars := ctx.Config().VendorConfig("acmeVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if acmeVars.IsSet(name) {
+			return acmeVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}

hentaiOS-picasso/platform_vendor_hentai

build/soong/generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func hentaiExpandVariables(ctx android.ModuleContext, in string) string {
+	hentaiVars := ctx.Config().VendorConfig("hentaiVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if hentaiVars.IsSet(name) {
+			return hentaiVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}